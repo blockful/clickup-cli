40500797c254495334d2a60dece4e1f2b4ca1e31
Alexandro T. Netto <56097505+alextnetto@users.noreply.github.com>
2026-02-16 22:01:51 +0000

    fix: wire ldflags version info from main to cmd package
    GoReleaser sets main.version/commit/date via ldflags but the vars
    lived in cmd package. Now main.go calls cmd.SetVersionInfo() to
    bridge them correctly.

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 	"github.com/blockful/clickup-cli/cmd"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	cmd.SetVersionInfo(version, commit, date)
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
